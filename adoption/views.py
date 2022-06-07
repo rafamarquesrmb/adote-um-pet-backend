48d5384c73415d3d110c5d66ef0ddaedbb134339
rafamarquesrmb <rafa.rmb@gmail.com>
2022-06-06 22:17:03 -0300

    Added POST for pet view and GET for Adoption View

--- adoption/views.py
+++ adoption/views.py
@@ -1,11 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 
 from adoption.serializers import AdoptionSerializer
 from .email_service import send_confirmation_email
+from .models import Adoption
+
 
 class AdoptionList(APIView):
+    def get(self, request, format=None):
+        adoptions = Adoption.objects.all()
+        serializer = AdoptionSerializer(adoptions, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
     def post(self, request, format=None):
         serializer = AdoptionSerializer(data=request.data)
         if serializer.is_valid():
@@ -14,7 +21,7 @@ class AdoptionList(APIView):
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(
             {
-                "errors": serializer.errors,
-                "message": "Something was wrong"
+                "message": "There were validation errors",
+                "errors": serializer.errors
             }
             , status=HTTP_400_BAD_REQUEST)
--- pet/views.py
+++ pet/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from .models import Pet
 from .serializers import PetSerializer
 
@@ -11,3 +11,13 @@ class PetList(APIView):
         pets = Pet.objects.all()
         serializer = PetSerializer(pets, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = PetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response({
+            "message": "There were validation errors",
+            "errors": serializer.errors
+        }, status=HTTP_400_BAD_REQUEST)
